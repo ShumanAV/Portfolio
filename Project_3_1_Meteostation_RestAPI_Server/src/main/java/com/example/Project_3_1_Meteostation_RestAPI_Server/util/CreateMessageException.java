9832622c52cc2e38087273a0960996a6493fd4ac
Alexandr <a_shuman@mail.ru>
2022-06-23 21:53:36 +0700

    add finish changes in project_3

--- Project_3_1_Meteostation_RestAPI_Server/src/main/java/com/example/Project_3_1_Meteostation_RestAPI_Server/util/CreateMessageException.java
+++ Project_3_1_Meteostation_RestAPI_Server/src/main/java/com/example/Project_3_1_Meteostation_RestAPI_Server/util/CreateMessageException.java
@@ -1,6 +1,6 @@
 package com.example.Project_3_1_Meteostation_RestAPI_Server.util;
 
-import com.example.Project_3_1_Meteostation_RestAPI_Server.util.exceptions.MeasurementException;
+import com.example.Project_3_1_Meteostation_RestAPI_Server.util.exceptions.SensorAndMeasurementException;
 import org.springframework.stereotype.Component;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.FieldError;
@@ -8,7 +8,7 @@ import java.util.List;
 
 // класс для формирования списка всех ошибок в данных запроса от клиента
 @Component
-public class ErrorsUtil {
+public class CreateMessageException {
 
     public static void returnErrorsToClient(BindingResult bindingResult) {
         StringBuilder errorMsg = new StringBuilder();
@@ -21,7 +21,7 @@ public class ErrorsUtil {
                     .append(" - ").append(error.getDefaultMessage()).append(";");
         }
         // выбросим нашу ошибку и передадим в нее текст ошибки валидации данных, нам остается ее поймать в другом методе
-        throw new MeasurementException(errorMsg.toString());
+        throw new SensorAndMeasurementException(errorMsg.toString());
     }
 
 }
--- Project_3_1_Meteostation_RestAPI_Server/src/main/java/com/example/Project_3_1_Meteostation_RestAPI_Server/util/exceptions/MeasurementException.java
+++
@@ -1,9 +0,0 @@
-package com.example.Project_3_1_Meteostation_RestAPI_Server.util.exceptions;
-
-// данный Exception о том, что в тех данных, которые прислал клиент, были ошибки
-public class MeasurementException extends RuntimeException{
-    public MeasurementException(String msg) {
-        // будем принимать сообщение об ошибке и передавать его в супер класс RuntimeException
-        super(msg);
-    }
-}
