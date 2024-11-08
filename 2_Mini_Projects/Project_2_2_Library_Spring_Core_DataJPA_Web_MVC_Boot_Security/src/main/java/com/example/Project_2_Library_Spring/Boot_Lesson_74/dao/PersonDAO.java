b106daba5304f6b0921a03db81d7ad7c6224414b
Алексендр Шуман <a_shuman@mail.ru>
2024-11-08 22:10:04 +0700

    The name of the second folder has been changed and minor changes have been made to the project.

--- 2_Mini_Projects/Project_2_2_Library_Spring_Core_DataJPA_Web_MVC_Boot_Security/src/main/java/com/example/Project_2_Library_Spring/Boot_Lesson_74/dao/PersonDAO.java
+++
@@ -1,8 +0,0 @@
-package com.example.Project_2_Library_Spring.Boot_Lesson_74.dao;
-
-import org.springframework.stereotype.Component;
-
-@Component
-public class PersonDAO {
-    //здесь будут лежать специфические SQL запросы к БД, пока все решаем в сервисе
-}
