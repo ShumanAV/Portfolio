1e73921a28bd938bba60a6ca4ad49ba0b0828ee1
Алексендр Шуман <a_shuman@mail.ru>
2024-11-08 22:22:55 +0700

    Change names of folders projects.

--- 2_Small_Projects/Project_2_1_Library_Spring_Core_DataJPA_Web_MVC_NoBoot/src/main/java/org/example/dao/PersonDAO.java
+++
@@ -1,8 +0,0 @@
-package org.example.dao;
-
-import org.springframework.stereotype.Component;
-
-@Component
-public class PersonDAO {
-    //здесь будут лежать специфические SQL запросы к БД, пока все решаем в сервисе
-}
